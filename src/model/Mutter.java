1ff25dbf46b64370f48cf1f9c5dcc84a9249f292
katsubehiroshi <katsubehiroshi@192.168.1.8>
2018-03-21 23:40:06 +0900

    Revert "Mutterクラスの変更"
    This reverts commit d55bc7bfdaa8bc829334dd1ce2ab7ce566a2eff2.

--- src/model/Mutter.java
+++ src/model/Mutter.java
@@ -2,24 +2,11 @@ package model;
 
 public class Mutter {
 	
-	private String id;
 	private String name;
 	private String content;
 	
 	public Mutter() {
 		
-	}
-	public Mutter(String id, String name, String content) {
-		this.id = id;
-		this.name = name;
-		this.content = content;
-	}
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
 	}
 	
 	public String getName() {
